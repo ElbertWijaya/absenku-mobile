bd874d6580369c748777f99b9fecbe5d5319179a
elbertw500@gmail.com <elbertw500@gmail.com>
2025-10-08 09:20:44 +0700

    chore: save work

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 
 void main() {
+=======
+import 'core/dio_client.dart';
+import 'features/auth/presentation/login_screen.dart';
+import 'features/home/presentation/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DioClient().init();
+>>>>>>> feat/mobile-login
   runApp(const AbsenkuApp());
 }
 
@@ -12,9 +22,17 @@ class AbsenkuApp extends StatelessWidget {
     return MaterialApp(
       title: 'Absenku',
       theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo),
+<<<<<<< HEAD
       home: const Scaffold(
         body: Center(child: Text('Absenku Mobile - fresh start')),
       ),
+=======
+      initialRoute: '/login',
+      routes: {
+        '/login': (_) => const LoginScreen(),
+        '/home': (_) => const HomeScreen(),
+      },
+>>>>>>> feat/mobile-login
     );
   }
 }
--- mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,17 @@ import FlutterMacOS
 import Foundation
 
 import flutter_secure_storage_macos
+<<<<<<< HEAD
 import mobile_scanner
+=======
+>>>>>>> feat/mobile-login
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+<<<<<<< HEAD
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+=======
+>>>>>>> feat/mobile-login
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
